abaf3bc4c9a9282165d3a335494bb7f5fa72d6da
Nathan Samson <nathan@nathansamson.be>
2021-08-23 20:28:02 +0200

    Add redirect to www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,20 @@
 class ApplicationController < ActionController::Base
   layout "application"
 
+  before_action :redirect_to_www
+
   include AuthenticatedSystem
   before_action do |c|
     @current_user = (c.session[:user] && User.find_by_id(c.session[:user])) || :false
   end
 
   protect_from_forgery
+
+  private
+
+  def redirect_to_www
+    if request.host == 'vlaamswoordenboek.be' && request.method == 'GET'
+      redirect_to "https://www.vlaamswoordenboek.be#{request.fullpath}", status: :moved_permanently
+    end
+  end
 end
